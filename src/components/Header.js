a76b67884173c63088465acadddbe26e78c0b6fe
Ashish Bhattarai <ashishbhattarai49@gmail.com>
2026-01-16 14:44:31 +0530

    fix: remove unused imports to satisfy eslint

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import { Avatar, Button, Stack } from "@mui/material";
+import { Button } from "@mui/material";
 import Box from "@mui/material/Box";
 import React, { useState } from "react";
 import "./Header.css";
